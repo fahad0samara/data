52bfbe7d1fcb1fea16dda61aa832347d2e8957e8
fahad0samarar <fah8ad@outlook.com>
2024-01-29 21:01:48 +0200

    add

---
+++ List.swift
@@ -0,0 +1,18 @@
+//
+//  List.swift
+//  data
+//
+//  Created by fahad samara on 1/29/24.
+//
+
+import SwiftUI
+
+struct List: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List()
+}
